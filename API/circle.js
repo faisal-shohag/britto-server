44718d184ca02415971b23266f0f4f88ad2f589c
faisal-shohag <faisalshohagprog@gmail.com>
2024-09-11 18:06:10 +0600

    profile

--- API/circle.js
+++ API/circle.js
@@ -87,6 +87,64 @@ router.get("/allposts/:userId", async (req, res) => {
   }
 });
 
+router.get("/user-posts/:userId", async (req, res) => {
+  const userId = parseInt(req.params.userId);
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const [posts, totalCount] = await Promise.all([
+      prisma.post.findMany({
+        where: { authorId: userId },
+        skip,
+        take: limit,
+        orderBy: {
+          createdAt: "desc",
+        },
+        include: {
+          author: true,
+          comments: {
+            select: {
+              id: true,
+            },
+          },
+          likes: {
+            select: {
+              id: true,
+              userId: true,
+            },
+          },
+          images: true,
+          // type: true,
+        },
+      }),
+      prisma.post.count(),
+    ]);
+
+  
+    const formattedPosts = posts.map((post) => {
+      return ({
+      ...post,
+      commentCount: post.comments.length,
+      likeCount: post.likes.length,
+      isLiked: post.likes.some((like) => like.userId === userId),
+      comments: undefined,
+      likes: undefined,
+    })});
+
+    res.json({
+      posts: formattedPosts,
+      currentPage: page,
+      totalPages: Math.ceil(totalCount / limit),
+      totalCount,
+    });
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    res.status(500).json({ error: "An error occurred while fetching posts" });
+  }
+});
+
 router.post("/posts/:postId/toggle-like", async (req, res) => {
   try {
     const { postId } = req.params;
